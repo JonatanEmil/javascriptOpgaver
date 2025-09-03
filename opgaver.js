afe7232b0ffea43b447be1a0ce39b4ac558de160
Jonatan Emil Rytter <jor001@edu.zealand.dk>
2025-09-03 13:33:06 +0200

    Opgave 4 done

--- opgaver.js
+++ opgaver.js
@@ -11,8 +11,9 @@ function sum(numberarray) {
     }
     return numbersum;
 }
-console.log(sum([1,2,3]));
-console.log(sum([10,20,30,40]));
+
+console.log(sum([1, 2, 3]));
+console.log(sum([10, 20, 30, 40]));
 console.log("");
 console.log("Opgave 2");
 
@@ -25,36 +26,91 @@ function max(numberarray) {
     }
     return numbermax;
 }
-console.log(max([1,2,3]));
+
+console.log(max([1, 2, 3]));
 console.log(max([20, 30, 10]));
 console.log("");
 console.log("Opgave 3");
 
-function countVowels(input){
+function countVowelsSmall(input) {
     let count = 0;
     for (let i = 0; i < input.length; i++) {
-        switch (input[i]) {
-            case 'a': count++;
-            break;
-            case 'e': count++;
-            break;
-            case 'i': count++;
-            break;
-            case 'o': count++;
-            break;
-            case 'u': count++;
-            break;
-            case 'y': count++;
-            break;
-            case 'æ': count++;
-            break;
-            case 'ø': count++;
-            break;
-            case 'å': count++;
-            break;
+        switch (input[i].toLowerCase()) {
+            case 'a':
+                count++;
+                break;
+            case 'e':
+                count++;
+                break;
+            case 'i':
+                count++;
+                break;
+            case 'o':
+                count++;
+                break;
+            case 'u':
+                count++;
+                break;
+            case 'y':
+                count++;
+                break;
+            case 'æ':
+                count++;
+                break;
+            case 'ø':
+                count++;
+                break;
+            case 'å':
+                count++;
+                break;
         }
     }
     return count;
 }
 
-console.log(countVowels("Alle mennesker skal leve"));
+console.log(countVowelsSmall('Åge bøjede syv fine rør ud.'));
+console.log('Virker ikke med caps');
+console.log('Opgave 3 med caps')
+
+function countVowels(input) {
+    let count = 0;
+    for (let i = 0; i < input.length; i++) {
+        const char = input[i].toLowerCase();
+        if (char == 'a' ||
+            char == 'e' ||
+            char == 'i' ||
+            char == 'o' ||
+            char == 'u' ||
+            char == 'y' ||
+            char == 'æ' ||
+            char == 'ø' ||
+            char == 'å') {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countVowels('Åge bøjede syv fine rør ud.'));
+console.log('Skulle bare bruge .toLowerCase, så ikke forskel på switch case og == (eller ===) i forhold til at læse store eller små bogstaver.');
+console.log("");
+console.log("Opgave 4");
+
+function filterOdd(numberarray) {
+    let oddarray = [];
+    for (let i = 0; i < numberarray.length; i++) {
+        const current = numberarray[i];
+        if (current%2 !== 0){
+            oddarray.push(current);
+        }
+    }
+    return oddarray
+}
+
+console.log(filterOdd([1, 2, 3, 4, 5]));
+console.log(filterOdd([10, 23, 37, 46, 52]));
+console.log("");
+console.log("Opgave 5");
+function reverseString(input) {
+    
+}
